530522244572bb50ed79740efd034d662376f335
gianfranco cherubini <cherussa@gmail.com>
2024-01-31 11:44:20 -0300

    agergando el dao de usuarios

---
+++ backend/src/dao/usuariosDao.js
@@ -0,0 +1,16 @@
+import { UsuariosModelo } from "./models/usuarios.model.js";
+
+export class UsuariosMongoDao {
+
+     async getUsuarios(usuarioId){
+
+        let usuarios = await UsuariosModelo.findById({_id: usuarioId, rol: "usuario"});
+        return usuarios;
+
+    }
+
+    async getUsuarioAdmin(adminId){
+        let administrador = await UsuariosModelo.findById({_id: adminId, rol: "administrador"});
+        return administrador;
+    }
+}
